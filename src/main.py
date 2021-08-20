e23fa286d14ea8b8d2271620ab4f543cb9d4186b
dunfrey@gmail.com <dunfrey@gmail.com>
2021-08-20 10:38:46 -0300

    storing results in instance of train and test class

--- src/main.py
+++ src/main.py
@@ -14,12 +14,17 @@ def read_csv_file(path, column_date, last_day, first_date=None):
     return df[(df[column_date] < last_day)][1:]
 
 
-db_folder = '../dbs/'
+db_folder = '/home/dunfrey/Documents/Doutorado UFRN/Artigos/araraquara/dbs/'
 data_limite = '2021-03-21'
 df_araraquara = read_csv_file(db_folder + 'df_araraquara.csv', 'date', data_limite, None)
 df_araraquara.confirmed = df_araraquara.confirmed.diff(7).dropna()
 df_araraquara.deaths = df_araraquara.deaths.diff(7).dropna()
 df_araraquara = df_araraquara.dropna()
+primeira_data = df_araraquara.date.min()
+df_parameters = read_csv_file(db_folder + 'df_sird_parameters.csv', 'Date', data_limite, primeira_data)
+df_principal_components_ori = read_csv_file(db_folder + 'principal_components_orig.csv', 'date', data_limite, primeira_data)
+df_principal_components_ant = read_csv_file(db_folder + 'principal_components_antec_lock.csv', 'date', data_limite, primeira_data)
+df_principal_components_not = read_csv_file(db_folder + 'principal_components_not_lock.csv', 'date', data_limite, primeira_data)
 
 # ---------------------
 
@@ -35,14 +40,15 @@ regressor = model_manner.build_model(model_config)
 
 
 regressor.fit_model(train, epochs=1, batch_size=16, verbose=0)
-yhat_train, rmse_scores_train = evaluator_manner.evaluate_model(regressor.model, train)
-yhat_test, rmse_scores_test = evaluator_manner.evaluate_model(regressor.model, test)
 
-print(sum(rmse_scores_test))
+train.y_hat, train.rmse = evaluator_manner.evaluate_model(regressor.model, train)
+test.y_hat, test.rmse = evaluator_manner.evaluate_model(regressor.model, test)
+
+print(sum(test.rmse))
 
 plt.rcParams["figure.figsize"] = (20, 5)
 plt.plot(train.x_label.reshape(train.x_label.shape[0], train.x_label.shape[1])[:, :1], label='original')
-plt.plot(yhat_train.reshape(yhat_train.shape[0], yhat_train.shape[1])[:, :1], label='predicted')
+plt.plot(train.y_hat.reshape(train.y_hat.shape[0], train.y_hat.shape[1])[:, :1], label='predicted')
 plt.show()
 #print(yhat_test[0].reshape(-1, 1))
 #plt.plot(yhat_test[0].reshape(-1, 1), label='predicted')
