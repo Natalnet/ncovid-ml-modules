7d420565fc5bf96eb67a3614b329b4ca84accaaa
davihdsantos <davihenriqueds@gmail.com>
2021-09-21 15:46:55 -0300

    fixed merge conflicts in routes.py

--- src/routes.py
+++ src/routes.py
@@ -6,21 +6,12 @@ import predictor_manner
 
 @app.route('/lstm/repo/<repo>/path/<path>/feature/<feature>/begin/<begin>/end/<end>/', methods=['GET'])
 def lstm(repo, path, feature, begin, end):
-<<<<<<< HEAD
-    resposta = predictor_manner.predict(repo, path, feature, begin, end)
-    return resposta
-
-#@app.route('/arima/repo/<repo>/path/<path>', methods=['GET'])
-
-# def ARIMA(variables):
-=======
     response = predictor_manner.predict(repo, path, feature, begin, end)
     return response
 
 #@app.route('/arima/repo/<repo>/path/<path>', methods=['GET'])
 
 # def arima(variables):
->>>>>>> 53002a1cad3cbe644d7ac9e14d4468b88b75f3bf
     # resposta = datamanager.predict(variables)
     # return resposta
 
