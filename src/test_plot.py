4e2bfce815bb49fc3da7faf573faa6bc0c39bbd1
davihdsantos <davihenriqueds@gmail.com>
2021-09-22 15:52:43 -0300

    added dates to x-axis of prediction plots

--- src/test_plot.py
+++ src/test_plot.py
@@ -13,15 +13,6 @@ import data_manner
 import evaluator_manner
 import plot_manner
 
-import datetime
-
-dt = datetime.datetime(2021, 12, 1)
-step = datetime.timedelta(days=1)
-print(dt.strftime('%Y-%m-%d'))
-
-for i in range(10):
-  print(dt - datetime.timedelta(days=i))
-
 def read_csv_file(path, column_date, last_day, first_date=None):
     df = pd.read_csv(path)
     if first_date is not None:
@@ -47,25 +38,7 @@ train, test = data_manner.build_data(data, step_size=week_size, size_data_test=2
 # model_config: [n_input, n_lstm_nodes, dropout, n_features]
 model_config = [week_size, 200, 0.0, train.x.shape[2]]
 
-# # ---------- Sample 1 - Without griding-search
-# regressor = model_manner.build_model(model_config)
-#
-# regressor.fit_model(train, epochs=1, batch_size=16, verbose=0)
-#
-# train.y_hat, train.rmse = evaluator_manner.evaluate_model(regressor.model, train)
-# test.y_hat, test.rmse = evaluator_manner.evaluate_model(regressor.model, test)
-#
-# print(sum(test.rmse))
-#
-# plt.rcParams["figure.figsize"] = (20, 5)
-# plt.plot(train.x_label.reshape(train.x_label.shape[0], train.x_label.shape[1])[:, :1], label='original')
-# plt.plot(train.y_hat.reshape(train.y_hat.shape[0], train.y_hat.shape[1])[:, :1], label='predicted')
-# plt.show()
-
-# ----------- Sample 2 - griding-search for a single configuration model based over the train data
-
 n_repeat = 2
-#grid_result_list = evaluator_manner.evaluate_model_n_repeat(n_repeat, model_config, train, epochs=10, batch_size=32, verbose=0)
 
 model_config_dict = {'n_input': [7],
                     'n_lstm_nodes': [100],
@@ -102,4 +75,4 @@ print(plot_manner.build_data_from_trained_model(test, gridsearch_result_list[0][
 
 model_example = gridsearch_result_list[0]['model_list'][0].model
 
-plot_manner.plot_predictions(train, model_example, last_date)
+plot_manner.plot_predictions(test, model_example, last_date)
