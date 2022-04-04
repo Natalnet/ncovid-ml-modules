634a5d30460e9454e46d18b1da75991ae4ca704c
Francinaldo Almeida <almeidafranci@users.noreply.github.com>
2022-04-04 10:27:57 -0300

    Update routes.py

--- src/routes.py
+++ src/routes.py
@@ -18,7 +18,7 @@ def lstm(repo, path, feature, begin, end):
         configs_manner.model_infos["model_id"], path, repo, feature, begin, end
     )
     response = predictor_obj.predict()
-    response_json = jsonify(response)
+    response_json = jsonify(predictor_obj.predictions_to_weboutput(response))
     return response_json
 
 
